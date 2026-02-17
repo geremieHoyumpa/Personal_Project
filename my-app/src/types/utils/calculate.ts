ed18f4690e437e7fb48c8149e9619067c5e9e793
Geremie Hoyumpa <geremie.hoyumpa23@gmail.com>
2026-02-17 15:54:34 +0800

    Add functions for calculating netprofit, break-even, profitmargin

--- my-app/src/types/utils/calculate.ts
+++ my-app/src/types/utils/calculate.ts
@@ -1,14 +1,25 @@
-import { BuisnessInput } from "../businessInput";
+import { BusinessInput } from "../businessInput";
 
-export const totalRevenue = (input:BuisnessInput) : number => {
+export const totalRevenue = (input:BusinessInput) : number => {
   return Number(input.revenuePerUnit) * Number(input.numberOfUnit);
 };
 
-const variableCost = (input:BuisnessInput) : number => {
+const variableCost = (input:BusinessInput) : number => {
   return Number(input.costPerUnit) * Number(input.numberOfUnit);
 };
 
-export const totalCost = (input:BuisnessInput) : number => {
+export const totalCost = (input:BusinessInput) : number => {
   return variableCost(input) + Number(input.fixedCost);
 };
 
+export const breakEvenPoint = (input:BusinessInput) : number => {
+  return Number(input.fixedCost) / (Number(input.revenuePerUnit) - Number(input.costPerUnit));
+};
+
+export const netProfit = (input:BusinessInput) : number => {
+  return totalRevenue(input) - totalCost(input);
+};
+
+export const profitMargin = (input:BusinessInput) : number => {
+  return (netProfit(input) / totalRevenue(input)) * 100;
+};
